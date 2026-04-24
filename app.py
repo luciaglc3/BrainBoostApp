feb6bd69e8cf25d51174a02ba0be0a331fa25d8c
luciaglc3 <6zhp7bxgcz@privaterelay.appleid.com>
2026-04-24 16:16:00 +0200

    Add English quiz route and evaluation logic

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from bottle import route, run, template
+from bottle import route, run, template, request
+import random
+from fragen import fragen_englisch
 
 @route('/')
 def startseite():
@@ -8,8 +10,28 @@ def startseite():
 def kategorien():
     return template('kategorien')
 
-run(host='localhost', port=8080, debug=True, reloader=True)
-
 @route('/quiz/allgemein')
 def quiz_allgemeinwissen():
     return template('allgemein')
+
+@route('/englisch')
+def englisch_test():
+	ausgewaehlte_fragen = random.sample(fragen_englisch, 10)
+	return template('quiz', titel="Englisch Quiz", fragen=ausgewaehlte_fragen)
+
+@route('/auswertung', method='POST')
+def auswertung():
+    punkte = 0
+
+    for i in range(5):
+        antwort = request.forms.get(f'frage{i}')
+        
+        if antwort:
+            if antwort == "RICHTIGE_ANTWORT":  # kommt gleich besser
+                punkte += 2
+
+    return f"<h1>Du hast {punkte} Punkte!</h1>"
+
+
+run(host='localhost', port=8080, debug=True, reloader=True)
+
