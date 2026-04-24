75151a384b8d1cb8bfca642775ac0e2406ba74e2
luciaglc3 <6zhp7bxgcz@privaterelay.appleid.com>
2026-04-24 15:12:23 +0200

    Add files via upload
    Fragen für Englisch Kategorie

---
+++ fragen.py
@@ -0,0 +1,13 @@
+
+fragen_englisch = [
+    {
+        "frage": "What is the past of 'go'?",
+        "antworten": ["goed", "went", "gone", "going"],
+        "richtig": "went"
+    },
+    {
+        "frage": "Which sentence is correct?",
+        "antworten": ["She go to school", "She goes to school", "She going to school", "She gone to school"],
+        "richtig": "She goes to school"
+    }
+]
