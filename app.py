2c09621b839829dd1cb85bcf918ed6cf4768a337
luciaglc3 <6zhp7bxgcz@privaterelay.appleid.com>
2026-04-28 22:50:51 +0200

    Increase sample size for quiz questions

--- app.py
+++ app.py
@@ -12,55 +12,24 @@ def kategorien():
 
 @route('/allgemeinwissen')
 def allgemein():
-    fragen = random.sample(fragen_allgemein, 10)
-    return template('quiz', titel="Allgemeinwissen Quiz", fragen=fragen)
+    fragen = random.sample(fragen_allgemein, 20)
+    return template('quiz', titel="Allgemeinwissen Quiz",kategorie="allgemein", fragen=fragen)
 
 @route('/mathe')
 def mathe():
-	fragen = random.sample(fragen_mathe, 10)
-	return template('quiz', titel="Mathe Quiz", fragen=fragen)
+	fragen = random.sample(fragen_mathe, 20)
+	return template('quiz', titel="Mathe Quiz",kategorie="mathe", fragen=fragen)
 
 
 @route('/englisch')
 def englisch_test():
-	ausgewaehlte_fragen = random.sample(fragen_englisch, 10)
-	return template('quiz', titel="Englisch Quiz", fragen=ausgewaehlte_fragen)
-
-@route('/test')
-def test():
-	fragen= { 
-	    "allgemein": random.sample(fragen_allgemein, 10),
-	    "mathe": random.sample(fragen_mathe, 10),
-	    "englisch": random.sample(fragen_englisch, 10)}
-
-	return template('test', fragen=fragen)
+	ausgewaehlte_fragen = random.sample(fragen_englisch, 20)
+	return template('quiz', titel="Englisch Quiz",kategorie="englisch", fragen=ausgewaehlte_fragen)
 
 
 
-@route('/auswertung', method='POST')
-def auswertung():
-    punkte = 0
-    gesamt = 0
-
-    for key in request.forms:
-        if key.startswith("antwort_"):
-            nummer = key.replace("antwort_", "")
-
-            gegebene_antwort = request.forms.get("antwort_" + nummer)
-            richtige_antwort = request.forms.get("richtig_" + nummer)
-
-            gesamt += 1
-
-            if gegebene_antwort == richtige_antwort:
-                punkte += 2
-
-    max_punkte = 60
-
-    return f"""
-    <h1>Ergebnis</h1>
-    <p>Du hast {punkte} von {max_punkte} Punkten erreicht.</p>
-    <p>Beantwortete Fragen: {gesamt} von 30</p>
-    <a href="/test">Test nochmal starten</a>
-    """
 	
+
+
+
 	run(host='localhost', port=8080, debug=True, reloader=True)
